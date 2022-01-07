61f91a0d24ac6c60742b293f5f2c55b7b66bfc64
tekabe1 <96458474+tekabe1@users.noreply.github.com>
2022-01-07 12:12:36 -0800

    nnn

--- practiceJava/src/practiceJava/JavaPrcts.java
+++ practiceJava/src/practiceJava/JavaPrcts.java
@@ -5,33 +5,40 @@ public class JavaPrcts {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-    int day=4;
-    String dayname;
-    switch(day)
-    {
-    case 1:
-    	dayname="Sunday";
-    	break;
-    case 2:
-    	dayname="Monday";
-    	break;
-    case 3:
-    	dayname="Tuesday";
-    	break;
-    case 4:
-    	dayname="Wedensday";
-    	break;
-    case 5:
-    	dayname="Thursday";
-        break;    	 
-    case 6:
-    	dayname="Friday";
-    	break;
-    default:
-    	dayname="Saturday";
-    	break; 
-       }
-	System.out.println(" Today's day  is " + dayname);
+                              // for loop practice
+		for(int i=1; i<=5; i++)
+			System.out.println(i);
+		
+		
+		/*
+		 * int num1=17; int num2=67; String message; if(num1<18 || num2>=80)
+		 * message="Needs help "; else message ="Doesn't need help"; System.out.println(
+		 * message);
+		 */
+		
+		
+		/*
+		 * int num1=18; int num2=45; String message; if(num1==18 && num2 <=40)
+		 * message="Adult"; else message ="Not an adult"; System.out.println("this is "
+		 * + message);
+		 */
+		
+		/*
+		 * int num1=6; String message; if(num1==2) message="Two"; else message =
+		 * "non-two"; System.out.println("this is " + message);
+		 */
+		 
+		
+		
+		
+		/*
+		 * int day=4; String dayname; switch(day) { case 1: dayname="Sunday"; break;
+		 * case 2: dayname="Monday"; break; case 3: dayname="Tuesday"; break; case 4:
+		 * dayname="Wedensday"; break; case 5: dayname="Thursday"; break; case 6:
+		 * dayname="Friday"; break; default: dayname="Saturday"; break; }
+		 * System.out.println(" Today's day  is " + dayname);
+		 */
+		 
     
     
 	}
